9fc81afaa91cd426affc4dbe22aac171d229d1b3
Vasilyev Ivan <vasilyev.ii@ya.ru>
2020-07-21 14:07:55 +0300

    Добавил использование QuizProps

--- hw03/src/main/java/ru/otus/dao/QuizDaoImpl.java
+++ hw03/src/main/java/ru/otus/dao/QuizDaoImpl.java
@@ -1,7 +1,8 @@
 package ru.otus.dao;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import ru.otus.configs.QuizProps;
 import ru.otus.model.Question;
 import ru.otus.model.Quiz;
 
@@ -14,37 +15,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class QuizDaoImpl implements QuizDao {
 
-    private final String questionsFileName;
-    private final String commaDelimiter;
-
-    public QuizDaoImpl(@Value("${quiz.questionsFileName}") String questionsFileName, @Value("${quiz.commaDelimiter}") String commaDelimiter) {
-        this.questionsFileName = questionsFileName;
-        this.commaDelimiter = commaDelimiter;
-    }
+    private final QuizProps quizProps;
 
     @Override
     public Quiz getQuiz() {
 
         List<Question> questions = new ArrayList<>();
 
-        URL url = ClassLoader.getSystemResource(questionsFileName);
+        URL url = ClassLoader.getSystemResource(quizProps.getQuestionsFileName());
         if (url == null) {
-            throw new QuizException("Questions file was not found: " + questionsFileName);
+            throw new QuizException("Questions file was not found: " + quizProps.getQuestionsFileName());
         }
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(new BufferedInputStream(url.openStream())))) {
             String line;
             while ((line = br.readLine()) != null) {
-                String[] v = line.split(commaDelimiter);
+                String[] v = line.split(quizProps.getCommaDelimiter());
                 if (v.length < 3) {
-                    throw new QuizException("Question have less than three fields: " + questionsFileName);
+                    throw new QuizException("Question have less than three fields: " + quizProps.getQuestionsFileName());
                 }
                 questions.add(new Question(v[0], v[1], v[2]));
             }
         } catch (IOException e) {
-            throw new QuizException("Questions file cannot be read: " + questionsFileName, e);
+            throw new QuizException("Questions file cannot be read: " + quizProps.getQuestionsFileName(), e);
         }
 
         return new Quiz(questions);
